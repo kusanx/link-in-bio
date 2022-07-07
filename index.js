202c1f84c3197c7ef225ee4c1042298353bc5d17
Samuel Hernandez <91435847+kusanx@users.noreply.github.com>
2022-07-07 00:50:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+VANTA.CLOUDS2({
+    el: "#clouds",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 150.00,
+    minWidth: 100.00,
+    scale: 0.5,
+    backgroundColor: 0xfcfafa,
+    skyColor: 0x162222,
+    cloudColor: 0xf0f11,
+    lightColor: 0xb6acac,
+    backgroundAlpha: 0.0,
+    texturePath: "./images/noise.png"
+  })
+
+  const main = document.querySelector('main');
+main.style.opacity = 1;
+
+})
+
